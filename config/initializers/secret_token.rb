33b91bfc1cea5ab9f1fb020738b082d19cb66344
Michael <silversteelwolf@gmail.com>
2014-03-17 14:55:58 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gwtrade::Application.config.secret_token = '95bb0fab4f2c5c20ac14dc153fdfaf194a8549bb83daec304f1e6540433f14ee981d336f31ff73f5fa8279fc9cd4ab9f47573c06fe1ca3000e4d6da2930e457e'
